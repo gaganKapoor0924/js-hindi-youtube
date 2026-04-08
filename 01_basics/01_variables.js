3636a0ee83e5317ff33b5344f32f34a45fadfc65
gaganKapoor0924 <gaganKapoor092485@gmail.com>
2026-04-08 06:50:52 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 2;
+let accountEmail = "gagan.kapoor987@gmail.com";
+var accountPassword = "123456";
+accountCity = "Jaipur";
+
+// accountId = 3;
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
