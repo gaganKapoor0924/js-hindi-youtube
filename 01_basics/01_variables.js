38071e18f31a1fc27be7fc34682e2e32c8bb2185
gaganKapoor0924 <gaganKapoor092485@gmail.com>
2026-04-08 06:55:21 +0000

    changes in variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -5,5 +5,8 @@ accountCity = "Jaipur";
 let accountState;
 
 // accountId = 3;
+accountEmail = "jatin.jatinKapoor@gmail.com";
+accountPassword = "345";
+accountCity = "Bengaluru";
 // console.log(accountId);
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
